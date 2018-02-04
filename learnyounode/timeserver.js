915b2a004c0eec1c8bfad8a5c873ab200c563ebf
Igor Cesar <igormarracho01@gmail.com>
2018-02-03 23:50:02 -0200

    timeserver.js done

---
+++ learnyounode/timeserver.js
@@ -0,0 +1,24 @@
+var net = require('net');
+
+var port = process.argv[2];
+
+function addZero(value){
+    if (value < 10){
+        return ('0' + value);
+    }
+    return value;
+}
+
+function currentDate() {
+    var date = new Date();
+    date = date.getFullYear() + '-' + addZero(date.getMonth() + 1) + '-' 
+    + addZero(date.getDate()) + ' ' + addZero(date.getHours()) + ':'
+    + addZero(date.getMinutes());
+    return date;
+}
+
+var server = net.createServer(function (socket) {
+    socket.write(currentDate() + '\n');
+    socket.end();
+})
+server.listen(port);
